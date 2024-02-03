fceb752258a4bb377bd8cc2d8dba8b12e96de692
no_name <25843689+eman-on@users.noreply.github.com>
2024-02-03 18:24:42 +0200

    Update VGTU_StudentBlock.user.js

--- student/Blocker/VGTU_StudentBlock.user.js
+++ student/Blocker/VGTU_StudentBlock.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VGTU - Student
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Block Students from surfing web
 // @author       mrNull
 // @match        https://*.*
